c275aeafb99ddacd8a09fd6c93a27f7ba1361ab6
innoink <innoink@163.com>
2018-01-05 11:23:09 +0800

    update gzip.hpp

--- utils/gzip.hpp
+++ utils/gzip.hpp
@@ -2,6 +2,7 @@
 #define GZIP_HPP
 
 #include <vector>
+#include <cstddef> //for size_t
 
 bool ungzip(std::vector<unsigned char>& in, size_t count_in, std::vector<unsigned char>& out, size_t& count_out);
 bool ungzip(std::vector<unsigned char>& in, std::vector<unsigned char>& out);
